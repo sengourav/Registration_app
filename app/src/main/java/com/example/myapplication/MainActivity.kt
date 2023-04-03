24153b813f0b8cab717a72fd8207b39a75cfad94
sengourav <107364930+sengourav@users.noreply.github.com>
2023-04-03 22:38:45 +0530

    Initiate commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,21 +2,36 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
-    val emailEditText=findViewById<EditText>(R.id.editTextTextEmailAddress)
-    val nameEditText=findViewById<EditText>(R.id.editTextTextPersonName)
-    val phoneEditText=findViewById<EditText>(R.id.editTextPhone)
-    val passwordEditText=findViewById<EditText>(R.id.editTextTextPassword)
-    val submit=findViewById<Button>(R.id.button2)
+    lateinit var binding:ActivityMainBinding
+    lateinit var database:DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        submit.setOnClickListener {
-            val user=User(nameEditText.text.toString(),emailEditText.text.toString(),phoneEditText.text.toString(),passwordEditText.text.toString())
+        binding.button2.setOnClickListener {
+            val user=User(binding.editTextTextPersonName.editableText.toString(),binding.editTextTextEmailAddress.editableText.toString(),binding.editTextPhone.editableText.toString(),binding.editTextTextPassword.editableText.toString())
+            database=FirebaseDatabase.getInstance().reference
+            database.child(binding.editTextTextPersonName.editableText.toString()).setValue(user).addOnSuccessListener {
+                Toast.makeText(this, "Successfully Registered", Toast.LENGTH_SHORT).show()
+               binding.editTextPhone.editableText.clear()
+                binding.editTextTextPassword.editableText.clear()
+                binding.editTextTextEmailAddress.editableText.clear()
+                binding.editTextTextPersonName.editableText.clear()
+
+            }.addOnFailureListener {
+                Toast.makeText(this, "Try Again or check your connection", Toast.LENGTH_SHORT).show()
+            }
+
 
         }
 
